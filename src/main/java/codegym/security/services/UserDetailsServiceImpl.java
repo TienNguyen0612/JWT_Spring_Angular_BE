be93d1ccb4da12ebf88853aa6042e155489e5529
TienNguyen0612 <sales08.taybactravel@gmail.com>
2022-04-01 12:03:44 +0700

    JWT Back-end

---
+++ src/main/java/codegym/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package codegym.security.services;
+
+import codegym.models.User;
+import codegym.repository.IUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    private IUserRepository iUserRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = iUserRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
+}
