d03284f6a83e883cbd016de3fea8a6d5d1f1026e
TienNguyen0612 <sales08.taybactravel@gmail.com>
2022-04-02 10:40:37 +0700

    fix syntax

--- src/main/java/codegym/controllers/AuthController.java
+++ src/main/java/codegym/controllers/AuthController.java
@@ -71,13 +71,13 @@ public class AuthController {
         if (iUserRepository.existsByUsername(signUpRequest.getUsername())) {
             return ResponseEntity
                     .badRequest()
-                    .body(new MessageResponse("Error: Username is already taken!"));
+                    .body(new MessageResponse("Error: Username is already taken !"));
         }
 
         if (iUserRepository.existsByEmail(signUpRequest.getEmail())) {
             return ResponseEntity
                     .badRequest()
-                    .body(new MessageResponse("Error: Email is already in use!"));
+                    .body(new MessageResponse("Error: Email is already in use !"));
         }
 
         // Create new user's account
@@ -118,6 +118,6 @@ public class AuthController {
         user.setRoles(roles);
         iUserRepository.save(user);
 
-        return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+        return ResponseEntity.ok(new MessageResponse("User registered successfully !"));
     }
 }
